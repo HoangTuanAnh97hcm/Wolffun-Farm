d9e4f017d95086ef98a5bfb51c9137fcb6661937
Hiienng <hoangtuananh.97.hcm@gmail.com>
2023-04-21 22:56:48 +0700

    Adjust AgriculturalSO

--- Assets/WolffunFarm/Scripts/ScriptableObject/AgriculturalSO.cs
+++ Assets/WolffunFarm/Scripts/ScriptableObject/AgriculturalSO.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,8 @@ using UnityEngine;
 public class AgriculturalSO : ScriptableObject 
 {
     public string nameString;
+    public int price;
+    public int totalProduct;
+    public float productTimeMinus;
     public Transform prefab;
-    public Transform visual;
 }
